c1523fdd9c285ea45adf92609217b876c906aad8
BadAtGames26 <125292563+BadAtGames26@users.noreply.github.com>
2026-02-18 19:18:36 -0700

    debug logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,52 @@
-#![feature(lazy_cell, ptr_sub_ptr)]
+#![feature(ptr_sub_ptr)]
+use engage::gamedata::{god::GodBond, unit::Unit, Gamedata, GodData, PersonData};
 use skyline::hooks::InlineCtx;
+use unity::{prelude::*, system::{Dictionary, List, Stack}};
+
+#[repr(C)]
+#[unity::class("App","GodBondHolder")]
+pub struct GodBondHolder {
+    parent: [u8; 0x10],
+    pub data: &'static GodData,
+    pub reliance: &'static u64,
+    pub bonds: &'static Dictionary<'static, Il2CppString, GodBond>,
+    pub pool: &'static PoolList<GodBond>
+}
+
+#[repr(C)]
+#[unity::class("App.Pool","List")]
+pub struct PoolList<T: 'static> {
+    pub list: &'static List<T>,
+    pub stack: &'static Stack<T>
+}
+
+#[unity::hook("App", "GodBondHolder", "Create")]
+pub fn gbh_create(this: &GodBondHolder, unit: &Unit, method_info: OptionalMethod) {
+    println!("GodBondHolder.Create:: Unit: {}, God: {}", unit.person.name.unwrap().to_string(), this.data.ascii_name.unwrap().to_string());
+    call_original!(this, unit, method_info);
+    //println!("GodBondHolder.Create:: DicLen: {}, DicCapacity: {}", this.bonds.get_count(), "?");
+    println!("GodBondHolder.Create:: PoolLen: {}, PoolCapacity: {}", this.pool.list.len(), this.pool.list.capacity());
+}
+
+#[unity::hook("App", "GodBond", "Clear")]
+pub fn gb_clear(this: &GodBond, method_info: OptionalMethod) {
+    println!("GodBond.Clear: Start");
+    call_original!(this, method_info);
+    let person = PersonData::get(this.pid);
+    if let Some(person) = person {
+        println!("GodBond.Clear: {} {}", person.name.unwrap().to_string(), this.god.ascii_name.unwrap().to_string());
+    }
+    println!("GodBond.Clear: End");
+}
 
 #[skyline::hook(offset=0x02b50e98, inline)]
 pub fn dictionary_hook(ctx: &mut InlineCtx) {
-    unsafe { *ctx.registers[1].x.as_mut() = 96; }     
+    ctx.registers[1].set_x(96);    
 }
 
 #[skyline::hook(offset=0x02b50ec0, inline)]
 pub fn pool_hook(ctx: &mut InlineCtx) {
-    unsafe { *ctx.registers[1].x.as_mut() = 96; }    
+    ctx.registers[1].set_x(96);    
 }
 
 #[skyline::main(name = "unitexp")]
@@ -40,5 +78,5 @@ pub fn main() {
             err_msg.as_str(),
         );
     }));
-    skyline::install_hooks!(dictionary_hook, pool_hook); 
+    skyline::install_hooks!(dictionary_hook, pool_hook, gbh_create, gb_clear); 
 }
